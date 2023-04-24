27dfc0fd5a04b58941cb22319544e679fcc632c4
soulee <me@soulee.dev>
2023-04-24 21:08:02 +0900

    add: ex02

--- 42cursus/CPP Module 01/ex02/main.cpp
+++ 42cursus/CPP Module 01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: soulee <soulee@studnet.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 21:01:52 by soulee            #+#    #+#             */
-/*   Updated: 2023/04/24 21:04:27 by soulee           ###   ########.fr       */
+/*   Updated: 2023/04/24 21:07:40 by soulee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,17 @@ int	main()
 	std::string	hello;
 	hello = "HI THIS IS BRAIN";
 
-	std::string	&stringPTR;
-	std::string	&stringREF;
+	std::string	*stringPTR = &hello;
+	std::string	&stringREF = hello;
 	
-	cout << &hello;
+	std::cout << &hello << std::endl;
+	std::cout << &stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << hello << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
 	return (1);
 }
