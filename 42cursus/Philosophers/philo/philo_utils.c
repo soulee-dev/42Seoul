fab906326a5180b14f7aa797f788cf98be3a223e
soulee <me@soulee.dev>
2023-05-01 12:27:32 +0900

    fix philosophers

--- 42cursus/Philosophers/philo/philo_utils.c
+++ 42cursus/Philosophers/philo/philo_utils.c
@@ -6,7 +6,7 @@
 /*   By: soulee <soulee@studnet.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/15 17:49:30 by soulee            #+#    #+#             */
-/*   Updated: 2023/05/01 12:09:48 by soulee           ###   ########.fr       */
+/*   Updated: 2023/05/01 12:19:50 by soulee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,4 +66,23 @@ void	print_mutex(t_philo_env *philo_env, t_philos *philos, char *s)
 	if (!is_finished(philo_env))
 		printf("%d %d %s\n", now - philo_env->time_start, philos->id + 1, s);
 	pthread_mutex_unlock(&(philo_env->print));
+}
+
+void	wait_time(int time, t_philo_env *philo_env)
+{
+	int	start;
+	int	now;
+
+	start = get_msec_now();
+	if (!start)
+		exit_error("[wait_time] time error");
+	while (!is_finished(philo_env))
+	{
+		now = get_msec_now();
+		if (!now)
+			exit_error("[wait_time] time error");
+		if (now - start >= time)
+			break ;
+		usleep(100);
+	}
 }
