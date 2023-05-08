385443fdd81979b8c85a79fcb945d594639c1bbe
soulee <me@soulee.dev>
2023-05-08 17:41:42 +0900

    add truncate_string to work

--- 42cursus/CPP Module 00/ex01/main.cpp
+++ 42cursus/CPP Module 00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: soulee <soulee@studnet.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 19:43:19 by soulee            #+#    #+#             */
-/*   Updated: 2023/04/24 19:43:19 by soulee           ###   ########.fr       */
+/*   Updated: 2023/05/08 17:40:57 by soulee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ std::string	truncate_string(std::string s)
 {
 	if (s.length() > 10)
 	{
-		s[9] = '.';
-		s[10] = 0;
+		s.resize(9);
+		s.push_back('.');
 	}
 	return (s);
 }
@@ -50,52 +50,54 @@ int	main()
 		{
 			if (phonebook.index > 0)
 			{
-					std::cout << "|     index|first_name| last_name|  nickname|" << std::endl;
+				std::cout << "|     index|first_name| last_name|  nickname|" << std::endl;
 				for (int i = 0; i < phonebook.index; i++)
 				{
 					print_spaces(i / 10);
 					std::cout << i;
 					std::cout << " ";
 
-					print_spaces(phonebook.GetContact(i).first_name.length());
-					std::cout << phonebook.GetContact(i).first_name;
+					print_spaces(truncate_string(phonebook.GetContact(i).first_name).length());
+					std::cout << truncate_string(phonebook.GetContact(i).first_name);
 					std::cout << " ";
 
-					print_spaces(phonebook.GetContact(i).last_name.length());
-					std::cout << phonebook.GetContact(i).last_name;
+					print_spaces(truncate_string(phonebook.GetContact(i).last_name).length());
+					std::cout << truncate_string(phonebook.GetContact(i).last_name);
 					std::cout << " ";
 
-					print_spaces(phonebook.GetContact(i).nickname.length());
-					std::cout << phonebook.GetContact(i).nickname;
+					print_spaces(truncate_string(phonebook.GetContact(i).nickname).length());
+					std::cout << truncate_string(phonebook.GetContact(i).nickname);
 					std::cout << std::endl;
 				}
 				std::cout << std::endl;
 				std::cout << "Input index: ";
 				std::cin >> index;
+				if (index >= 0 && index < phonebook.index)
+				{
+					index = index % 8;
+					std::cout << "|     index|first_name| last_name|  nickname|" << std::endl;
+					print_spaces(index / 10);
+					std::cout << index;
+					std::cout << " ";
 
-				index = index % 8;
-				std::cout << "|     index|first_name| last_name|  nickname|" << std::endl;
-				print_spaces(index / 10);
-				std::cout << index;
-				std::cout << " ";
+					print_spaces(truncate_string(phonebook.GetContact(index).first_name).length());
+					std::cout << truncate_string(phonebook.GetContact(index).first_name);
+					std::cout << " ";
 
-				print_spaces(phonebook.GetContact(index).first_name.length());
-				std::cout << phonebook.GetContact(index).first_name;
-				std::cout << " ";
+					print_spaces(truncate_string(phonebook.GetContact(index).last_name).length());
+					std::cout << truncate_string(phonebook.GetContact(index).last_name);
+					std::cout << " ";
 
-				print_spaces(phonebook.GetContact(index).last_name.length());
-				std::cout << phonebook.GetContact(index).last_name;
-				std::cout << " ";
-
-				print_spaces(phonebook.GetContact(index).nickname.length());
-				std::cout << phonebook.GetContact(index).nickname;
-				std::cout << std::endl;
+					print_spaces(truncate_string(phonebook.GetContact(index).nickname).length());
+					std::cout << truncate_string(phonebook.GetContact(index).nickname);
+					std::cout << std::endl;
+				}
+				else
+					std::cerr << "Out Of Index" << std::endl;
 			}
 		}
 		else if (cmd == "EXIT")
-		{
 			break ;
-		}
 	}
 	return (0);
 }
