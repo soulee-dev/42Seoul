1d8a9162de38c0ab519ca8d9e7d693f5e06c2d6d
soulee <me@soulee.dev>
2023-06-07 18:53:24 +0900

    add cpp module 02 ex 00

--- 42cursus/CPP Module 02/ex00/Fixed.hpp
+++ 42cursus/CPP Module 02/ex00/Fixed.hpp
@@ -6,19 +6,28 @@
 /*   By: soulee <soulee@studnet.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 20:18:46 by soulee            #+#    #+#             */
-/*   Updated: 2023/06/06 20:28:01 by soulee           ###   ########.fr       */
+/*   Updated: 2023/06/07 18:52:27 by soulee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 # define FIXED_HPP
 
+# include <iostream>
+
 class Fixed
 {
 	private:
-		int	fixed_point;
-		
-}
+		int					value;
+		const static int	bits = 8;
+	public:
+		Fixed(void);
+		Fixed(const Fixed &ref);
+		~Fixed();
+		Fixed	&operator=(const Fixed &ref);
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+};
 
 
 #endif
--- 42cursus/CPP Module 02/ex00/main.cpp
+++ 42cursus/CPP Module 02/ex00/main.cpp
@@ -6,15 +6,23 @@
 /*   By: soulee <soulee@studnet.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 20:18:35 by soulee            #+#    #+#             */
-/*   Updated: 2023/06/06 20:19:21 by soulee           ###   ########.fr       */
+/*   Updated: 2023/06/07 18:51:19 by soulee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include "Fixed.hpp"
 
 int	main(void)
 {
 	Fixed	a;
 	Fixed	b(a);
 	Fixed	c;
+
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+
+	return (0);
 }
