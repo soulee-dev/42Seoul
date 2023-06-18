7d5ba4ba19bacfb2a17a7c6e2b046bd1b2f2571a
soulee <me@soulee.dev>
2023-06-18 21:18:32 +0900

    add example

--- 42cursus/CPP Module 07/ex00/main.cpp
+++ 42cursus/CPP Module 07/ex00/main.cpp
@@ -6,21 +6,27 @@
 /*   By: soulee <soulee@studnet.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/18 21:05:52 by soulee            #+#    #+#             */
-/*   Updated: 2023/06/18 21:13:03 by soulee           ###   ########.fr       */
+/*   Updated: 2023/06/18 21:17:39 by soulee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "whatever.hpp"
 
-int	main(void)
-{
-	int	a = 5;
-	int	b = 7;
+int main( void ) {
+	int a = 2;
+	int b = 3;
 
-	std::cout << a << " : " << b << std::endl;
-	std::cout << max(a, b) << min(a, b) << std::endl;
-	swap(a, b);
-	std::cout << a << " : " << b << std::endl;
-	return (0);
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
 }
