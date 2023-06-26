8e02efb57437f13299ab931127aa576bec73a7a6
soulee <me@soulee.dev>
2023-06-26 17:13:17 +0900

    add spaces after include keyword

--- 42cursus/CPP Module 08/ex00/easyfind.hpp
+++ 42cursus/CPP Module 08/ex00/easyfind.hpp
@@ -6,15 +6,15 @@
 /*   By: soulee <soulee@studnet.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 12:31:16 by soulee            #+#    #+#             */
-/*   Updated: 2023/06/26 14:32:47 by soulee           ###   ########.fr       */
+/*   Updated: 2023/06/26 14:43:25 by soulee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
-#include <iostream>
-#include <vector>
+# include <iostream>
+# include <vector>
 
 template <typename T>
 typename	T::iterator	easyfind(T& container, int val)
